150eaf2f9c46ebe4b2f2a4d394fed960fc4390e3
Daniel McGillick <56147357+DMacMakes@users.noreply.github.com>
2026-01-01 00:24:38 +1100

    Proper p5 project setup for vs code extn. purple fat lines.

--- sketch.js
+++ sketch.js
@@ -22,8 +22,10 @@ function draw() {
   image(img, 0, 0);
 
   // draw grid
-  stroke(0, 80);
-  let step = 50;
+  let linecol = color(255, 0, 255, 80); 
+  stroke(linecol);
+  let step = 120;
+  strokeWeight(3);
   for (let x = 0; x < img.width; x += step) line(x, 0, x, img.height);
   for (let y = 0; y < img.height; y += step) line(0, y, img.width, y);
 
@@ -34,14 +36,14 @@ function mouseWheel(e) {
   zoom *= e.delta > 0 ? 0.9 : 1.1;
 }
 
-function touchMoved() {
+/*function touchMoved() {
   if (touches.length === 1) {
     // drag
     offsetX += movedX;
     offsetY += movedY;
   }
   return false;
-}
+}*/
 
 function touchStarted() {
   if (touches.length === 2) {
@@ -53,7 +55,11 @@ function touchStarted() {
 }
 
 function touchMoved() {
-  if (touches.length === 2) {
+  if (touches.length === 1) {
+    // drag
+    offsetX += movedX;
+    offsetY += movedY;
+  } else if (touches.length === 2) {
     let d = dist(
       touches[0].x, touches[0].y,
       touches[1].x, touches[1].y
